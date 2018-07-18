119dad192b92ec05905d3dad71221fbde5990dce
lokimanosfr <lokimanosfr@gmail.com>
2018-07-19 02:15:55 +0500

    Working features: set create value create parameter get parameter replace substring of parameter delete key delete value

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
@@ -13,41 +15,63 @@ import (
 )
 
 type commandArgs struct {
-	path    string
-	value   string
-	set     string
-	replace string
-	get     string
-	contain string
-	help    bool
+	path        string
+	value       string
+	set         string
+	replace     string
+	get         bool
+	contain     string
+	help        bool
+	valType     string
+	deleteKey   string
+	deleteValue string
+	createKey   string
 }
 
-const (
-	Read  = "read"
-	Write = "write"
-)
-
 var (
-	args = commandArgs{}
+	args     = commandArgs{}
+	regTypes = map[string]uint32{
+		"DWORD":     registry.DWORD,
+		"QWORD":     registry.QWORD,
+		"SZ":        registry.SZ,
+		"MULTI_SZ":  registry.MULTI_SZ,
+		"EXPAND_SZ": registry.EXPAND_SZ,
+	}
+	Access = map[string]uint32{
+		"read":   registry.QUERY_VALUE,
+		"write":  registry.QUERY_VALUE | registry.SET_VALUE,
+		"create": registry.QUERY_VALUE | registry.CREATE_SUB_KEY,
+		"delete": registry.QUERY_VALUE | registry.SET_VALUE,
+	}
 )
 
 func init() {
 
 	flag.StringVar(&args.path, "path", "", "Set the path to registry key")
 	flag.StringVar(&args.value, "value", "", "Set which value use")
+	flag.StringVar(&args.deleteKey, "delkey", "", "Will delete subkey(value) from path")
+	flag.StringVar(&args.deleteValue, "delval", "", "Will delete value of subkey ")
 	flag.StringVar(&args.set, "set", "", "Set params to value")
-	flag.StringVar(&args.replace, "replace", "", "Repalce param or substring to another")
-	flag.StringVar(&args.get, "get", "", "Get parametrs of value")
+	flag.StringVar(&args.replace, "replace", "", "Replace param or substring to another")
+	flag.BoolVar(&args.get, "get", false, "Get parametrs of value")
 	flag.StringVar(&args.contain, "contain", "", "Chek that the value contain param")
+	flag.StringVar(&args.valType, "type", "", "Used when need create value")
+	flag.StringVar(&args.createKey, "createkey", "", "create subkey")
 	flag.BoolVar(&args.help, "help", false, "Show usage")
 	flag.Parse()
 	if args.help {
 		fmt.Println("Usage:")
-		fmt.Println("-path <Path to key> -val <Value Name> -set <param>\t	| Set params to value")
-		fmt.Println("-path <Path to key> -val <Value Name> -set <[param;param]>\t| Usage for REG_MULTI_SZ. Set params to value")
-		fmt.Println("-path <Path to key> -val <Value Name> -replace <param>=><param>\t| Usage for replace param or substring to another. ")
-		fmt.Println("-path <Path to key> -val <Value Name> -get\t		| Get parametrs of value")
-		fmt.Println("-path <Path to key> -val <Value Name> -contain <param>\t	| fing param and return true if it's finded or false if not")
+		fmt.Println("-path <Path to key> -value <Value Name> -set <param>\t| Set params to value")
+		fmt.Println("-path <Path to key> -value <Value Name> -type <value type> -set <param>\t| Set params to value even it doesn't exist ( will create value)")
+		fmt.Println("-path <Path to key> -value <Value Name> -set \"<[param;param]>\"\t|Usage for REG_MULTI_SZ. Set params to value")
+		fmt.Println("-path <Path to key> -value <Value Name> type <value type> -set <[param;param]>\t| Usage for REG_MULTI_SZ. Set params to value even it doesn't exist ( will create value)")
+		fmt.Println("-path <Path to key> -value <Value Name> -replace <param>=><param>\t| Usage for replace param or substring to another. ")
+		fmt.Println("-path <Path to key> -value <Value Name> -get\t| Get parametrs of value")
+		fmt.Println("-path <Path to key> -value <Value Name> -contain <param>\t| fing param and return true if it's finded or false if not")
+		fmt.Println("-path <Path to key>  -delkey <key>\t| Will delete key from path")
+		fmt.Println("-path <Path to key>  -delval <value>\t| Will delete value from path ")
+		fmt.Println("-path <Path to key>  -createkey <key>\t| Will create key ")
+		fmt.Println("-value <DWORD,QWORD,SZ,MULTI_SZ,EXPAND_SZ>\t| Types for params ")
 		os.Exit(0)
 	}
 
@@ -55,47 +79,94 @@ func init() {
 
 func main() {
 
-	// str := "Hello fucking world good world buy macbook world"
-	// fndstr := "w*.rl.?"
-	// re := regexp.MustCompile(fndstr)
-	// ss := re.ReplaceAllString(str, "BAD")
-	// fmt.Println(ss)
-
-	// return
-
-	if args.path == "" || args.value == "" {
+	if args.path == "" {
 		fmt.Println("invalid path and value, use -help")
 		return
 	}
 	switch arg := args.chekArgs(); arg {
 	case "set":
-		spl := getSplitedParams()
-		err := setParams(args.path, args.value, spl)
+
+		i, typ, err := setParams(args.path, args.value, []string{args.set})
 		if err != nil {
 			fmt.Println(err.Error())
+			return
 		}
+		switch typ {
+		case 11:
+			strUint := strconv.FormatUint(getUint64FromInterface(i), 10)
+			fmt.Println("Set " + strUint + " to " + args.path + "\\" + args.value + " successful")
+		case 4:
+			strUint := strconv.FormatUint(uint64(getUint32FromInterface(i)), 10)
+			fmt.Println("Set " + strUint + " to " + args.path + "\\" + args.value + " successful")
+		case 1, 2:
+			fmt.Println("Set " + getStringFromInterface(i) + " to " + args.path + "\\" + args.value + " successful")
+		case 7:
+			arr := strings.Join(getStringsFromInterface(i), ";")
+			fmt.Println("Set " + arr + " to " + args.path + "\\" + args.value + " successful")
+		default:
+
+		}
+
 	case "replace":
-		fmt.Println("USE SET")
+		was, now, typ, err := replaceParams(args.path, args.value, []string{args.replace})
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println("Replaced successful!")
+		switch typ {
+		case regTypes["SZ"], regTypes["EXPAND_SZ"]:
+			fmt.Println("Was: " + getStringFromInterface(was))
+			fmt.Println("Now: " + getStringFromInterface(now))
+		case regTypes["MULTI_SZ"]:
+			fmt.Println("Was: " + strings.Join(getStringsFromInterface(was), ";"))
+			fmt.Println("Now: " + strings.Join(getStringsFromInterface(now), ";"))
+
+		}
 	case "get":
-		// i, t := getParams(args.path, args.value)
+		i, typ := getParams(args.path, args.value)
+		switch typ {
+		case 11:
+			fmt.Println(getUint64FromInterface(i))
+		case 4:
+			fmt.Println(getUint32FromInterface(i))
+		case 1, 2:
+			fmt.Println(getStringFromInterface(i))
+		case 7:
+			fmt.Println(getStringsFromInterface(i))
+		default:
+			fmt.Println(args.path + "\\" + args.value + " is not exist")
+
+		}
 	case "contain":
-		fmt.Println("USE SET")
+
+	case "create":
+		fmt.Println(createKey(args.path, args.createKey))
+
+	case "delkey":
+		fmt.Println(deleteKey(args.path, args.deleteKey))
+	case "delval":
+		fmt.Println(deleteValue(args.path, args.deleteValue))
 	default:
 		fmt.Println("invaid argument, use -help")
 	}
 
 }
 
-func getSplitedParams() []string {
+func getSplitedParams(argSet string) []string {
 	var spl []string
-	if string(args.set[0]) == "[" && string(args.set[len(args.set)-1]) == "]" {
-		spl := strings.Split(string(args.set[1:len(args.set)-1]), ";")
-		setParams(args.path, args.value, spl)
+	var out []string
+	if string(argSet[0]) == "[" && string(argSet[len(argSet)-1]) == "]" && strings.Contains(argSet, ";") {
+		spl = strings.Split(string(argSet[1:len(argSet)-1]), ";")
+		for _, p := range spl {
+			if p != "" {
+				out = append(out, p)
+			}
+		}
 	} else {
-		spl = append(spl, args.set)
-
+		out = append(out, argSet)
 	}
-	return spl
+	return out
 }
 
 func (args *commandArgs) chekArgs() string {
@@ -105,21 +176,30 @@ func (args *commandArgs) chekArgs() string {
 	if args.replace != "" {
 		return "replace"
 	}
-	if args.get != "" {
+	if args.get != false {
 		return "get"
 	}
 	if args.contain != "" {
 		return "contain"
 	}
+	if args.deleteKey != "" {
+		return "delkey"
+	}
+	if args.deleteValue != "" {
+		return "delval"
+	}
+	if args.createKey != "" {
+		return "create"
+	}
+
 	return ""
 }
 
-func getValueType(fullPath, value string) uint32 {
+func getExistValueType(fullPath, value string) uint32 {
 
-	key := openKey(fullPath, Read)
+	key := openKey(fullPath, Access["read"])
 	_, typ, err := key.GetValue(value, make([]byte, 0, 0))
 	if err != nil {
-		fmt.Println(err.Error())
 		return 0
 	}
 	defer key.Close()
@@ -128,57 +208,75 @@ func getValueType(fullPath, value string) uint32 {
 }
 
 //TODO: Доделать
-func findParam(fullPath, value string, param []string) bool {
-	typ := getValueType(fullPath, value)
-	if typ == 0 {
-		fmt.Println("param is not exist")
-		return false
-	}
+// func findParam(fullPath, value string, param []string) bool {
+// 	typ := getValueType(fullPath, value)
+// 	if typ == 0 {
+// 		fmt.Println("param is not exist")
+// 		return false
+// 	}
 
-	return true
+// 	return true
+// }
+
+func getStringFromInterface(i interface{}) string {
+	return i.(string)
+}
+func getUint64FromInterface(i interface{}) uint64 {
+	v := reflect.ValueOf(i)
+	return v.Interface().(uint64)
+}
+func getUint32FromInterface(i interface{}) uint32 {
+	v := reflect.ValueOf(i)
+	return v.Interface().(uint32)
+}
+func getStringsFromInterface(i interface{}) []string {
+	src := reflect.ValueOf(i)
+	srcArr := src.Interface().([]string)
+	return srcArr
+
 }
 
-func replaceParams(fullPath, value string, param []string) string {
+func replaceParams(fullPath, value string, param []string) (was interface{}, now interface{}, typ uint32, err error) {
 	argsToReplace := strings.Split(param[0], "=>")
-	typ := getValueType(fullPath, value)
+	typ = getExistValueType(fullPath, value)
 	if typ == 0 {
-		fmt.Println("param is not exist")
-		return ""
+		return nil, nil, 0, errors.New(value + " is not exist")
 	}
 
 	srcInterface, _ := getParams(fullPath, value)
 	switch typ {
 	case 1:
-		src := srcInterface.(string)
+		src := getStringFromInterface(srcInterface)
 		re := regexp.MustCompile(argsToReplace[0])
 		ss := re.ReplaceAllString(src, argsToReplace[1])
-		setParams(fullPath, value, []string{ss})
-		return ss
+		i, typ, err := setParams(fullPath, value, []string{ss})
+		return srcInterface, i, typ, err
 	case 2:
-		src := srcInterface.(string)
+		src := getStringFromInterface(srcInterface)
 		re := regexp.MustCompile(argsToReplace[0])
 		ss := re.ReplaceAllString(src, argsToReplace[1])
-		setParams(fullPath, value, []string{ss})
-		return ss
-	case 4:
-		fmt.Println("Wron type of value(you try set the )")
-	case 11:
-		fmt.Println("Wron type of value(you try set the )")
+		i, typ, err := setParams(fullPath, value, []string{ss})
+		return srcInterface, i, typ, err
 	case 7:
-		fmt.Println("Wron type of value(you try set the )")
+		src := getStringsFromInterface(srcInterface)
+		re := regexp.MustCompile(argsToReplace[0])
+		var outArr []string
+		for _, line := range src {
+			ss := re.ReplaceAllString(line, argsToReplace[1])
+			outArr = append(outArr, ss)
+		}
+		i, typ, err := setParams(fullPath, value, []string{"[" + strings.Join(outArr, ";") + "]"})
+		return srcInterface, i, typ, err
+	default:
+		return nil, nil, 0, errors.New("Can't replace DWORD,QWORD,BINARY parametrs of value, only set")
 	}
-	return ""
 
 }
 
-func setParams(fullPath, value string, param []string) error {
-	typ := getValueType(fullPath, value)
-	if typ == 0 {
-		fmt.Println("param is not exist")
-	}
+func set(fullPath, value string, valType uint32, param []string) (interface{}, error) {
 	var err error
-	key := openKey(fullPath, Write)
-	switch typ {
+	key := openKey(fullPath, Access["write"])
+	switch valType {
 	case 1:
 		err = key.SetStringValue(value, param[0])
 	case 2:
@@ -186,34 +284,88 @@ func setParams(fullPath, value string, param []string) error {
 	case 4:
 		i, err2 := strconv.ParseUint(param[0], 10, 32)
 		if err2 != nil {
-			return err2
+			return nil, err2
 		}
 		err = key.SetDWordValue(value, uint32(i))
 	case 11:
 		i, err2 := strconv.ParseUint(param[0], 10, 64)
 		if err2 != nil {
-			return err2
+			return nil, err2
 		}
 		err = key.SetQWordValue(value, uint64(i))
 	case 7:
 		err = key.SetStringsValue(value, param)
+
 	}
 	key.Close()
-	return err
+	i, _ := getParams(fullPath, value)
+	return i, err
+}
+
+func setParams(fullPath, value string, param []string) (interface{}, uint32, error) {
+	spl := getSplitedParams(param[0])
+	typ := getExistValueType(fullPath, value)
+	var i interface{}
+	var err error
+	if typ == 0 {
+		if args.valType != "" {
+			i, err = set(fullPath, value, regTypes[args.valType], spl)
+			return i, typ, err
+		}
+		fmt.Println("Value is not exist, use -help to know how create value")
+
+	} else {
+		i, err = set(fullPath, value, typ, spl)
+	}
+	return i, typ, err
+}
+
+func deleteKey(fullPath, path string) string {
+	key := openKey(fullPath, Access["delete"])
+	err := registry.DeleteKey(key, path)
+	if err != nil {
+		return "Key " + path + " not deleted:\n" + err.Error()
+	}
+	return "Key " + path + " delete successful"
+}
+
+func deleteValue(fullPath, value string) string {
+	key := openKey(fullPath, Access["delete"])
+	err := key.DeleteValue(value)
+	if err != nil {
+		return "Value " + value + " not deleted:\n" + err.Error()
+	}
+	return "Value " + value + " deleted successful"
 
 }
 
+func createKey(fullPath, subKey string) string {
+	key := openKey(fullPath, Access["create"])
+
+	_, opened, err := registry.CreateKey(key, subKey, registry.CREATE_SUB_KEY)
+	if err != nil {
+		return err.Error()
+	}
+	if opened {
+		return "Key alredy exist"
+	}
+	return fullPath + "\\" + subKey + " created"
+}
+
 func getParams(fullPath, value string) (interface{}, uint32) {
-	typ := getValueType(fullPath, value)
+	typ := getExistValueType(fullPath, value)
 	if typ == 0 {
 		return nil, 0
 	}
-	key := openKey(fullPath, Read)
+	key := openKey(fullPath, Access["read"])
 	switch typ {
 	case 1, 2:
 		str, _, _ := key.GetStringValue(value)
 		return str, typ
-	case 4, 11:
+	case 4:
+		num, _, _ := key.GetIntegerValue(value)
+		return uint32(num), typ
+	case 11:
 		num, _, _ := key.GetIntegerValue(value)
 		return num, typ
 	case 7:
@@ -231,16 +383,10 @@ func getKeyPath(fullPath string) string {
 	return strings.Join(strings.Split(fullPath, "\\")[1:], "\\")
 }
 
-func openKey(fullPath string, operation string) registry.Key {
+func openKey(fullPath string, access uint32) registry.Key {
 	var k registry.Key
 	var err error
-	var access uint32
-	switch operation {
-	case "write":
-		access = registry.QUERY_VALUE | registry.SET_VALUE
-	case "read":
-		access = registry.QUERY_VALUE
-	}
+
 	switch hkey := getHKEY(fullPath); hkey {
 	case "HKEY_LOCAL_MACHINE":
 		k, err = registry.OpenKey(registry.LOCAL_MACHINE, getKeyPath(fullPath), access)
@@ -254,10 +400,8 @@ func openKey(fullPath string, operation string) registry.Key {
 		k, err = registry.OpenKey(registry.CURRENT_CONFIG, getKeyPath(fullPath), access)
 	}
 	if err != nil {
-		fmt.Println("Operation " + operation)
-		log.Fatal(err)
+		log.Fatal(err.Error() + "\npath = " + fullPath)
 	}
 
 	return k
-
 }
--- main_test.go
+++ main_test.go
@@ -8,59 +8,16 @@ import (
 	"golang.org/x/sys/windows/registry"
 )
 
-func Test_setParams(t *testing.T) {
-	type args struct {
-		fullPath string
-		value    string
-		param    []string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name:    "DWORD",
-			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "DWORD_TEST", []string{"10"}},
-			wantErr: false,
-		},
-		{
-			name:    "QWORD",
-			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "QWORD_TEST", []string{"11"}},
-			wantErr: false,
-		},
-		{
-			name:    "EXPAND_SZ",
-			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"12"}},
-			wantErr: false,
-		},
-		{
-			name:    "MULTI_SZ",
-			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "MULTI_SZ_TEST", []string{"13", "14"}},
-			wantErr: false,
-		},
-		{
-			name:    "SZ",
-			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"15"}},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := setParams(tt.args.fullPath, tt.args.value, tt.args.param); (err != nil) != tt.wantErr {
-				t.Errorf("setParams() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func Test_getParams(t *testing.T) {
 
-	err := setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"hello great -Xmx=10G"})
-	if err != nil {
-		fmt.Println("set err + " + err.Error())
-	}
-	err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"hello great -Xmx=10G hello  -Xmx=10Ggreat"})
+	_, _, err := setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"12"})
+	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"15"})
+
+	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "MULTI_SZ_TEST", []string{"[13;14]"})
+
+	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "DWORD_TEST", []string{"10"})
+
+	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "QWORD_TEST", []string{"11"})
 	if err != nil {
 		fmt.Println("set err + " + err.Error())
 	}
@@ -77,7 +34,7 @@ func Test_getParams(t *testing.T) {
 		{
 			name:  "DWORD",
 			args:  args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "DWORD_TEST"},
-			want:  uint64(10),
+			want:  uint32(10),
 			want1: registry.DWORD,
 		},
 		{
@@ -118,20 +75,47 @@ func Test_getParams(t *testing.T) {
 	}
 }
 
-func Test_replaceParams(t *testing.T) {
-	err := setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"hello great -Xmx=10G"})
-	if err != nil {
-		fmt.Println("set err + " + err.Error())
+func Test_getSplitedParams(t *testing.T) {
+	type args struct {
+		argSet string
 	}
-	err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"hello great -Xmx=10G hello  -Xmx=10Ggreat"})
-	if err != nil {
-		fmt.Println("set err + " + err.Error())
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "array",
+			args: args{"[1;2;3;4]"},
+			want: []string{"1", "2", "3", "4"},
+		},
+		{
+			name: "string",
+			args: args{"[1;2;3;4"},
+			want: []string{"[1;2;3;4"},
+		},
+		{
+			name: "has empty string",
+			args: args{"[1,2,3;332;1,2,3;[123];;;;;,;\\;;,;]"},
+			want: []string{"1,2,3", "332", "1,2,3", "[123]", ",", "\\", ","},
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getSplitedParams(tt.args.argSet); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getSplitedParams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+func Test_createKey(t *testing.T) {
+	path := "HKEY_LOCAL_MACHINE\\SOFTWARE\\Google\\Chrome"
+	key := "NEWKEY"
+	fmt.Println(deleteKey(path, key))
 	type args struct {
 		fullPath string
-		value    string
-		param    []string
+		underKey string
 	}
 	tests := []struct {
 		name string
@@ -139,21 +123,147 @@ func Test_replaceParams(t *testing.T) {
 		want string
 	}{
 		{
-			name: "EXPAND_SZ",
-			args: args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"-Xmx=.*=>-Xmx=2G"}},
-			want: "hello great -Xmx=2G",
-		},
-		{
-			name: "SZ",
-			args: args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"-Xmx=.*=>-Xmx=1G"}},
-			want: "hello great -Xmx=1G",
+			name: "Create key",
+			args: args{path, key},
+			want: path + "\\" + key + " created",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := replaceParams(tt.args.fullPath, tt.args.value, tt.args.param); got != tt.want {
-				t.Errorf("replaceParams() = %v, want %v", got, tt.want)
+			if got := createKey(tt.args.fullPath, tt.args.underKey); got != tt.want {
+				t.Errorf("createKey() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
+
+func Test_setParams(t *testing.T) {
+	type args struct {
+		fullPath string
+		value    string
+		param    []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		typ     uint32
+		wantErr bool
+	}{
+		{
+			name:    "DWORD",
+			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "DWORD_TEST", []string{"10"}},
+			wantErr: false,
+			want:    uint32(10),
+			typ:     4,
+		},
+		{
+			name:    "QWORD",
+			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "QWORD_TEST", []string{"11"}},
+			wantErr: false,
+			want:    uint64(11),
+			typ:     11,
+		},
+		{
+			name:    "EXPAND_SZ",
+			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"12"}},
+			wantErr: false,
+			want:    string("12"),
+			typ:     2,
+		},
+		{
+			name:    "MULTI_SZ",
+			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "MULTI_SZ_TEST", []string{"[13;14]"}},
+			wantErr: false,
+			want:    []string{"13", "14"},
+			typ:     7,
+		},
+		{
+			name:    "SZ",
+			args:    args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"15"}},
+			wantErr: false,
+			want:    string("15"),
+			typ:     1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := setParams(tt.args.fullPath, tt.args.value, tt.args.param)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("setParams() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				switch got1 {
+				case 1:
+					t.Errorf("setParams() got = %v, want %v", getStringFromInterface(got), tt.want)
+				case 7:
+					t.Errorf("setParams() got = %v, want %v", getStringsFromInterface(got), tt.want)
+				case 2:
+					t.Errorf("setParams() got = %v, want %v", getStringFromInterface(got), tt.want)
+				case 4:
+					t.Errorf("setParams() got = %v, want %v", getUint32FromInterface(got), tt.want)
+				case 11:
+					t.Errorf("setParams() got = %v, want %v", getUint64FromInterface(got), tt.want)
+				}
+			}
+			if got1 != tt.typ {
+				t.Errorf("setParams() got1 = %v, want %v", got1, tt.typ)
+			}
+		})
+	}
+}
+
+// func Test_replaceParams(t *testing.T) {
+// 	_, _, err := setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"hello great -Xmx=10G"})
+// 	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"hello great -Xmx=10G hello  -Xmx=10Ggreat"})
+// 	_, _, err = setParams("HKEY_CURRENT_CONFIG\\Software\\Fonts", "MULTI_SZ_TEST", []string{"[hello great ;;;-Xmx=10G hello  -Xmx=10Ggreat;hello great -Xmx=10G hello  -Xmx=10Ggreat;hello great -Xmx=10G hello  -Xmx=10Ggreat]"})
+// 	type args struct {
+// 		fullPath string
+// 		value    string
+// 		param    []string
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		wantWas interface{}
+// 		wantNow interface{}
+// 		wantTyp uint32
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "EXPAND_SZ",
+// 			args: args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "EXPAND_SZ_TEST", []string{"-Xmx=.*=>-Xmx=2G"}},
+// 			want: "hello great -Xmx=2G replace successful",
+// 		},
+// 		{
+// 			name: "SZ",
+// 			args: args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "SZ_TEST", []string{"-Xmx=.*=>-Xmx=1G"}},
+// 			want: "hello great -Xmx=1G replace successful",
+// 		},
+// 		{
+// 			name: "MULTI_SZ",
+// 			args: args{"HKEY_CURRENT_CONFIG\\Software\\Fonts", "MULTI_SZ_TEST", []string{"great=>OLO"}},
+// 			want: "replace successful",
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotWas, gotNow, gotTyp, err := replaceParams(tt.args.fullPath, tt.args.value, tt.args.param)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("replaceParams() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(gotWas, tt.wantWas) {
+// 				t.Errorf("replaceParams() gotWas = %v, want %v", gotWas, tt.wantWas)
+// 			}
+// 			if !reflect.DeepEqual(gotNow, tt.wantNow) {
+// 				t.Errorf("replaceParams() gotNow = %v, want %v", gotNow, tt.wantNow)
+// 			}
+// 			if gotTyp != tt.wantTyp {
+// 				t.Errorf("replaceParams() gotTyp = %v, want %v", gotTyp, tt.wantTyp)
+// 			}
+// 		})
+// 	}
+// }
